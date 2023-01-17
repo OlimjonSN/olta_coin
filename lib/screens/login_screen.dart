887e2607ab1ab975f44d0e4b7c59d86d91ffdef2
RustambekSafarov <rustambeksafarovv@gmail.com>
2023-01-17 15:24:37 +0500

    login_screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,11 +1,40 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:olta_coin/models/add_color.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: prBackgroundColor,
+          child: LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    height: constraints.maxHeight * 0.65,
+                    child: Image.asset('images/olta.png'),
+                  ),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.1,
+                    width: MediaQuery.of(context).size.width - 40,
+                    child: Text(
+                      'Fastest chain wallet, anytime, anywhere Your Asset, Your Control',
+                      style: TextStyle(color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
