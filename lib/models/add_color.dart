acdfca949642692b1addc5c996012b21056690a2
Olimjon <olimjonsn230@gmail.com>
2023-01-17 11:28:20 +0500

    updated color

--- lib/models/add_color.dart
+++ lib/models/add_color.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-appColor() {
-  Color prBackgroundColor = const Color(0xFF000000);
-  Color white = Colors.white;
-  Color blue = Colors.blue;
-  Color secondBackgroundColorA = const Color(0xFF383838);
-  Color secondBackgroundColorB = const Color(0xFF636363);
-  Color buttonColorA = const Color(0xFF16EF72);
-  Color buttonColorB = const Color(0xFF0DF4BF);
-  Color textFieldColor = const Color(0xFF141414);
-  Color smallTextColor = const Color(0xFFc7c7c7);
-  Color secondTextFieldColor = const Color(0xFF001724);
-}
+Color prBackgroundColor = const Color(0xFF000000);
+Color secondBackgroundColorA = const Color(0xFF383838);
+Color secondBackgroundColorB = const Color(0xFF636363);
+Color buttonColorA = const Color(0xFF16EF72);
+Color buttonColorB = const Color(0xFF0DF4BF);
+Color textFieldColor = const Color(0xFF141414);
+Color smallTextColor = const Color(0xFFc7c7c7);
+Color secondTextFieldColor = const Color(0xFF001724);
